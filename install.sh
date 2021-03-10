857335a0802caa6ef45a7eda0b33b5e53a783687
Mario Manno <mario.manno@suse.com>
2021-03-10 17:52:01 +0100

    Use docker-compose build and own coturn server
    * Don't use the images from docker.io, build locally.
    * Bump docker-compose version

--- install.sh
+++ install.sh
@@ -1,5 +1,24 @@
-apt update
-apt install docker-compose
+#!/bin/false
+
+# apt update
+# apt install docker-compose
+# apt remove docker-compose
+# docker-compose 1.28 needed
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# lets encrypt storage
 mkdir /srv/le
 chmod 600 /srv/le
-cp -i .env.template .env
+
+# build needs a user account
+adduser build
+adduser build docker
+cd ~build
+git clone https://github.com/thecodingmachine/workadventure
+
+cd workadventure
+cp -i ~/.env .
+cp -i ~/docker-compose.yaml .
+sudo -u build docker-compose build
+sudo -u build docker-compose start
